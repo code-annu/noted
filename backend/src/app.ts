f62ac527e5789a65b0372d0cd7ceeefdc84aa91e
Anurag Rajput <code.annu@gmail.com>
2025-11-04 22:02:10 +0530

    implement auth feature

---
+++ backend/src/app.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import { connectDB } from "./infrastructure/config/db";
+import { authRouter } from "./api/router/auth-router";
+import { errorHandler } from "./api/middleware/handle-error";
+
+const app = express();
+const BASE_API_URL = "/api/v1";
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get(`${BASE_API_URL}/test`, (req, res) => {
+  res.send("yes tested");
+});
+app.use(`${BASE_API_URL}/auth`, authRouter);
+
+app.use(errorHandler);
+
+const PORT = 3000;
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(
+      `Server is listening at http://localhost:${PORT}${BASE_API_URL}`
+    );
+  });
+});
