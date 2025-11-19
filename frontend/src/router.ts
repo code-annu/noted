40d5a92983164b957e6fa2922c34e132749151ab
Anurag Rajput <code.annu@gmail.com>
2025-11-19 22:53:51 +0530

    implement user profile page

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -3,6 +3,7 @@ import SignupPage from "./presentation/pages/auth/SignupPage";
 import LoginPage from "./presentation/pages/auth/LoginPage";
 import { HomePage } from "./presentation/pages/dashboard/HomePage";
 import DashboardLayout from "./presentation/layouts/DashboardLayout";
+import ProfilePage from "./presentation/pages/dashboard/ProfilePage";
 
 export enum AppRoute {
   HOME = "/",
@@ -16,7 +17,10 @@ export const appRouter = createBrowserRouter([
   {
     path: AppRoute.HOME,
     Component: DashboardLayout,
-    children: [{ index: true, Component: HomePage }],
+    children: [
+      { index: true, Component: HomePage },
+      { path: `${AppRoute.HOME}/:username`, Component: ProfilePage },
+    ],
   },
   { path: AppRoute.SIGNUP, Component: SignupPage },
   { path: AppRoute.LOGIN, Component: LoginPage },
