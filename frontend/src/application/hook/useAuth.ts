fca9047c6b97e37702b40643d0064bfaaf828a25
Anurag Rajput <code.annu@gmail.com>
2025-11-19 22:31:22 +0530

    implement auth application layer

---
+++ frontend/src/application/hook/useAuth.ts
@@ -0,0 +1,47 @@
+import { useDispatch } from "react-redux";
+import { AuthRepository } from "../../data/repository/AuthRepository";
+import type {
+  LoginCredential,
+  SignupCredential,
+} from "../../domain/entities/user";
+import { LoginUsecase } from "../usecase/auth/LoginUsecase";
+import { SignupUsecase } from "../usecase/auth/SignupUsecase";
+import { setUser } from "../../features/auth/authSlice";
+import { StorageUtil } from "../../util/StorageUtil";
+import { RefreshTokenUsecase } from "../usecase/auth/RefreshTokenUsecase";
+
+function useAuth() {
+  const authRepo = new AuthRepository();
+  const dispatch = useDispatch();
+
+  const signup = async (cred: SignupCredential) => {
+    const signupUsecase = new SignupUsecase(authRepo);
+    const authUser = await signupUsecase.execute(cred);
+    dispatch(setUser({ user: authUser }));
+    StorageUtil.saveAccessToken(authUser.accessToken);
+    StorageUtil.saveRefreshToken(authUser.refreshToken);
+  };
+
+  const login = async (cred: LoginCredential) => {
+    const loginUsecase = new LoginUsecase(authRepo);
+    const authUser = await loginUsecase.execute(cred);
+    dispatch(setUser({ user: authUser }));
+    StorageUtil.saveAccessToken(authUser.accessToken);
+    StorageUtil.saveRefreshToken(authUser.refreshToken);
+  };
+
+  const refreshToken = async () => {
+    const refreshTokenUsecase = new RefreshTokenUsecase(authRepo);
+    const token = StorageUtil.getRefreshToken();
+    if (token) {
+      const authUser = await refreshTokenUsecase.execute(token);
+      dispatch(setUser({ user: authUser }));
+      StorageUtil.saveAccessToken(authUser.accessToken);
+      StorageUtil.saveRefreshToken(authUser.refreshToken);
+    }
+  };
+
+  return { signup, login, refreshToken };
+}
+
+export default useAuth;
