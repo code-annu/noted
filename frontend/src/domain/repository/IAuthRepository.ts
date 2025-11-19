94fda9dbdffd97ad19946bafae95688686e7366c
Anurag Rajput <code.annu@gmail.com>
2025-11-19 22:31:06 +0530

    implement auth domain layer

--- frontend/src/domain/repository/IAuthRepository.ts
+++ frontend/src/domain/repository/IAuthRepository.ts
@@ -2,6 +2,8 @@ import type { LoginCredential, SignupCredential, User } from "../entities/user";
 
 export interface IAuthRepository {
   signupUser(signupCred: SignupCredential): Promise<User>;
+
   loginUser(loginCred: LoginCredential): Promise<User>;
+
   refreshToken(token: string): Promise<User>;
 }
